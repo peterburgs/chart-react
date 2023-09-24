99d2ec59816d46d48b0ab50618da706a950b6741
Peter Burgs <tmanh3@cmcglobal.vn>
2023-09-25 05:56:19 +0700

    initial commit

---
+++ src/scenes/navbar/index.tsx
@@ -0,0 +1,49 @@
+import { Box, Typography, useTheme } from "@mui/material";
+import { FlexBetween } from "../../components/FlexBetween";
+import { useState } from "react";
+import PixIcon from "@mui/icons-material/Pix";
+import { Link } from "react-router-dom";
+
+export const Navbar = () => {
+  const { palette } = useTheme();
+  const [selected, setSelected] = useState("dashboard");
+
+  return (
+    <FlexBetween mb={"0.25rem"} p={"0.5rem 0rem"} color={palette.grey[300]}>
+      <FlexBetween gap={"0.75rem"}>
+        <PixIcon
+          sx={{
+            fontSize: "28px",
+          }}
+        />
+        <Typography>Finanseer</Typography>
+      </FlexBetween>
+      <FlexBetween gap="2rem">
+        <Box sx={{ "&:hover": { color: palette.primary[100] } }}>
+          <Link
+            to="/"
+            onClick={() => setSelected("dashboard")}
+            style={{
+              color: selected === "dashboard" ? "inherit" : palette.grey[700],
+              textDecoration: "inherit",
+            }}
+          >
+            dashboard
+          </Link>
+        </Box>
+        <Box sx={{ "&:hover": { color: palette.primary[100] } }}>
+          <Link
+            to="/predictions"
+            onClick={() => setSelected("predictions")}
+            style={{
+              color: selected === "predictions" ? "inherit" : palette.grey[700],
+              textDecoration: "inherit",
+            }}
+          >
+            predictions
+          </Link>
+        </Box>
+      </FlexBetween>
+    </FlexBetween>
+  );
+};
